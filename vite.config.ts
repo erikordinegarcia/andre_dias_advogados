a396381fb94bdb4fcd47d0c7a214c010c8c15460
erik.garciaordine@hotmail.com <erik.garciaordine@hotmail.com>
2026-02-26 17:59:23 -0300

    Site

--- vite.config.ts
+++ vite.config.ts
@@ -153,6 +153,7 @@ function vitePluginManusDebugCollector(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector()];
 
 export default defineConfig({
+  base: "/andre_dias_advogados/",
   plugins,
   resolve: {
     alias: {
